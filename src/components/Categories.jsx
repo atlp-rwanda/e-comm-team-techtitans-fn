eaa1cc483846c1ebbbcf7997c4c9c346c1610717
Gentille <ernegentille@gmail.com>
2023-05-19 16:45:31 +0300

    chore(redux setup): redux setup

---
+++ src/components/Categories.jsx
@@ -0,0 +1,11 @@
+import "./Header/Header.css";
+
+let Categories=() =>{
+  return (
+    <div className='homeCont'>
+      <h1>This is Categories Page </h1>
+    </div>
+  );
+};
+
+export default Categories
