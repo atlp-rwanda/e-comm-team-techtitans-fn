69aa49e368545dea73d80fda37eeacd928e405f0
NIYONIZEYE Faustin <niyof97@gmail.com>
2023-06-05 10:38:05 +0200

    feature(products):Show recommended products

--- src/Redux/Store/index.js
+++ src/Redux/Store/index.js
@@ -1,24 +1,25 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import resetPasswordReducer from '../Features/passwordResetSlice';
-import verifyotpSlice from '../Features/verifyotp.slice';
-import CategoryReducer from '../../Redux/Features/Product/CategorySlice';
-import productReducer from '../../Redux/Features/Product/AddProductSlice';
-import loginReducer from '../Features/User/loginSlice';
-import AlluserSlice from '../Features/User/viewUser/view.slice';
-import setRoleSlice from '../Features/User/viewUser/setRole.slice';
-import SignupSlice from '../../Redux/Features/signup/SignupSlice';
-import setAccountStatusReducer from '../Features/User/accountStatusSlice';
-import productsReviewSlice from '../../Redux/Features/Review/ProductReview';
-import singleProductReducer from '../Features/Dashboard/singleProductSlice';
-import productsReducer from '../Features/Dashboard/productsSlice';
-import ProductDetailsReducer from '../Features/Product/EditProductSlice';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import resetPasswordReducer from "../Features/passwordResetSlice";
+import verifyotpSlice from "../Features/verifyotp.slice";
+import CategoryReducer from "../../Redux/Features/Product/CategorySlice";
+import productReducer from "../../Redux/Features/Product/AddProductSlice";
+import loginReducer from "../Features/User/loginSlice";
+import AlluserSlice from "../Features/User/viewUser/view.slice";
+import setRoleSlice from "../Features/User/viewUser/setRole.slice";
+import SignupSlice from "../../Redux/Features/signup/SignupSlice";
+import setAccountStatusReducer from "../Features/User/accountStatusSlice";
+import productsReviewSlice from "../../Redux/Features/Review/ProductReview";
+import singleProductReducer from "../Features/Dashboard/singleProductSlice";
+import productsReducer from "../Features/Dashboard/productsSlice";
+import ProductDetailsReducer from "../Features/Product/EditProductSlice";
 import { singleUserReducer } from "../Features/User/getOneUserSlice";
+import RecommendedProduct from "../Features/Product/RecommendedSlice";
 const middleware = [...getDefaultMiddleware(), thunk];
 
 // eslint-disable-next-line no-undef
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
 }
 
@@ -38,6 +39,7 @@ const store = configureStore({
     products: productsReducer,
     productDetails: ProductDetailsReducer,
     oneuser: singleUserReducer,
+    recommended: RecommendedProduct,
   },
   middleware,
 });
--- src/components/Product/Card.jsx
+++ src/components/Product/Card.jsx
@@ -1,43 +1,58 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getProducts } from "../../Redux/Features/Dashboard/productsSlice";
 import "./Product.scss";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Card = () => {
   const dispatch = useDispatch();
-  const { products } = useSelector((state) => state.products);
+  const { products, isLoading } = useSelector((state) => state.products);
   useEffect(() => {
     dispatch(getProducts());
   }, [dispatch]);
+  if (isLoading === true || products === null) {
+    return (
+      <div>
+        <Backdrop
+          sx={{ color: "#7A89E9", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={open}
+          onClick={() => {}}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    );
+  }
+
   return (
     <>
-    
       <div className="card-container">
-        
         {products.length > 0 ? (
-        products.map((product) => (
+          products.map((product) => (
             <div className="card-one">
-            <img src={product.images[0]} />
-            <div className="card-title">
-            <div className="product-title">
-              <p>{product.name}</p>
-              <h5>${product.price}</h5>
-            </div>
+              <Link to={`/buyer/product/${product.id}`} key={product.id}>
+                <div>
+                  <img src={product.images[0]} />
+                </div>
+              </Link>
+              <div className="card-title">
+                <div className="product-title">
+                  <p>{product.name}</p>
+                  <h5>${product.price}</h5>
+                </div>
 
-            <div className="icon">
-              <FavoriteBorderIcon />
+                <div className="icon">
+                  <FavoriteBorderIcon />
+                </div>
+              </div>
             </div>
-          </div>
-            </div>
-        ))
-      ) : (
-        <div>Loading products...</div>
-      )}
-         
-   
-        
-
+          ))
+        ) : (
+          <div>Loading products...</div>
+        )}
       </div>
     </>
   );
