db942a1ec66f66006975b9ca14717ad575714ffe
EdwinBayingana <bayinganaedwin@gmail.com>
2023-06-19 15:01:56 +0200

    remove the timeout on the page with the CardDetailsForm

--- src/Redux/Features/User/viewUser/setRole.slice.js
+++ src/Redux/Features/User/viewUser/setRole.slice.js
@@ -1,35 +1,34 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
-import { BASE_URL } from "../../../../utils/apiUtilis";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { BASE_URL } from '../../../../utils/apiUtilis';
 
 export const addRoles = createAsyncThunk(
-  "api/v1/user/role",
+  'api/v1/user/role',
   async ({ id, email, roleId }, { rejectWithValue }) => {
     try {
-      const authToken = localStorage.getItem("token");
+      const authToken = localStorage.getItem('token');
       const configs = {
         headers: {
-          Authorization: "Bearer " + authToken,
+          Authorization: 'Bearer ' + authToken,
         },
       };
       const response = await axios.put(
         `${BASE_URL}/api/v1/user/role/${id}`,
         { email, roleId },
-        configs
+        configs,
       );
 
       return {
         data: response.data.data,
       };
     } catch (error) {
-      console.log("Fetch users error:", error.response.data.message);
       return rejectWithValue(error.response.data.message);
     }
-  }
+  },
 );
 
 const setRoleSlice = createSlice({
-  name: "setRole",
+  name: 'setRole',
   initialState: {
     setRole: null,
     status: null,
@@ -37,15 +36,15 @@ const setRoleSlice = createSlice({
   },
   extraReducers: {
     [addRoles.pending]: (state) => {
-      state.status = "loading.....";
+      state.status = 'loading.....';
     },
     [addRoles.fulfilled]: (state, action) => {
       state.setRole = action.payload;
-      state.status = "success";
+      state.status = 'success';
     },
     [addRoles.rejected]: (state, action) => {
       state.error = action.payload;
-      state.status = "failed";
+      state.status = 'failed';
     },
   },
 });
